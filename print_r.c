4471cc1e33c660e126771cafd9d5d56bf9a48fc9
Waridi318 <rosenjeri7151@gmail.com>
2023-01-26 09:34:51 -0800

    print_r.c

---
+++ print_r.c
@@ -0,0 +1,106 @@
+#include "main.h"
+
+/**
+ * print_bigS - Non printable chars
+ * @l: va_list args from _printf
+ * @f: pointer to the struct flags
+ * Return: number of char printed
+ */
+
+int print_bigS(va_list l, flags_t *f)
+{
+	int i, count = 0;
+	char *res;
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		return (_puts("(null)"));
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
+		{
+			_puts("\\x");
+			count += 2;
+			res = convert(s[i], 16, 0);
+			if (!res[1])
+				count += _putchar('0');
+			count += _puts(res);
+		}
+		else
+			count += _putchar(s[i]);
+	}
+	return (count);
+}
+
+/**
+ * print_rev - prints a str in reverse
+ * @l: arg from _printf
+ * @f: *p -> struct flags that determines
+ * Return: len(printed str)
+ */
+
+int print_rev(va_list l, flags_t *f)
+{
+	int i = 0, j;
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		s = "(null)";
+
+	while (s[i])
+		i++;
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+
+	return (i);
+}
+
+/**
+ * print_rot13 - prints a str using rot13
+ * @l: list of args from _printf
+ * @f: *p -> struct flags that determines
+ * Return: len(printed str)
+ */
+
+int print_rot13(va_list l, flags_t *f)
+{
+	int i, j;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	for (j = 0; s[j]; j++)
+	{
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
+		{
+			for (i = 0; i <= 52; i++)
+			{
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
+			}
+		}
+	}
+
+	return (j);
+}
+
+/**
+ * print_percent - prints a percent
+ * @l: va_list args from _printf
+ * @f: *p -> struct flags in which we turn the flags on
+ * Return: number of char printed
+ */
+
+int print_percent(va_list l, flags_t *f)
+{
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
+}
