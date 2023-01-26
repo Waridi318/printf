dfc1d3a13b6d1da0bfca6db02ad2125da543311c
Waridi318 <rosenjeri7151@gmail.com>
2023-01-26 05:21:08 -0800

    _writechar.c

---
+++ _writechar.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the char to stdout
+ * @c: The char to print
+ * Return: 1
+ */
+
+int _putchar(char c)
+{
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _puts - prints a str to stdout
+ * @str: pointer to the str to print
+ * Return: number of chars written
+ */
+
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
