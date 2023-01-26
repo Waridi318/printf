64af8239a9f2384ff107da46ed8e40f6f3040ee9
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-26 09:27:40 -0800

    add print_address.c

---
+++ print_address.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_address - prints address of input in hexa format
+  * @l: va_list args from _printf
+  * @f: *p -> struct flags that determines
+  * Return: number of char printed
+  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	char *str;
+	unsigned long int p = va_arg(l, unsigned long int);
+
+	register int count = 0;
+
+	(void)f;
+	if (!p)
+		return (_puts("(nil)"));
+
+	str = convert(p, 16, 1);
+	count += _puts("0x");
+	count += _puts(str);
+	return (count);
+}
