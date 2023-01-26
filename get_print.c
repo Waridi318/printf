7dafab6969207ad0bf668a9345431fddaa13d3f3
Waridi318 <rosenjeri7151@gmail.com>
2023-01-26 05:23:10 -0800

    get_print.c

---
+++ get_print.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * get_print - selects the right printing func
+ * @s: char that holds the conversion specifier
+ * Return: a pointer to the matching printing function
+ */
+
+int (*get_print(char s))(va_list, flags_t *)
+{
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
+
+	register int i;
+
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
+}
